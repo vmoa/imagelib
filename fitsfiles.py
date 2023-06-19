546cc38b56e82a678bd400e4dbb60c4f30f06990
David Kensiski <David@Kensiski.org>
2023-06-18 18:40:32 -0700

    Create sqlite database from fits files (not an in-memory dictionary

--- fitsfiles.py
+++ fitsfiles.py
@@ -15,177 +15,190 @@ import fitsdb
 
 class FitsFiles:
 
-	imagedb = list()
-	name_ndx = dict()
-	date_ndx = dict()
+    imagedb = list()
+    name_ndx = dict()
+    date_ndx = dict()
 
-	forcepng = False
+    forcepng = False
 
-	# Build this from file?
-	ngc_catalog = {
-		'NGC 1952': 'M1',
-		'NGC 5457': 'M101',
-	}
+    # Build this from file?
+    ngc_catalog = {
+        'NGC 1952': 'M1',
+        'NGC 5457': 'M101',
+    }
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def fetchFits(self, path):
-		'''Descend into `path` and build a database (dictionary for now) of all our FITS images.'''
-		pass
+    def fetchFits(self, path):
+        '''Descend into `path` and build a database (dictionary for now) of all our FITS images.'''
+        pass
 
-	def parseFilename(self, filename):
-		'''Parse `filename` and return a dictionary with the deets.'''
-		image = {}
-		name = []
+    def parseFilename(self, filename):
+        '''Parse `filename` and return a dictionary with the deets.'''
+        image = {}
+        name = []
 
-		# First let's strip the .fits
-		filename = os.path.splitext(filename)[0]
-		fn_re = re.compile(' +')
-		fn = fn_re.split(filename)
+        # First let's strip the .fits
+        filename = os.path.splitext(filename)[0]
+        fn_re = re.compile(' +')
+        fn = fn_re.split(filename)
 
-		# Work through filename components and see what matches
-		# What doesn't match should be part of the target name
-		for thang in fn:
+        # Work through filename components and see what matches
+        # What doesn't match should be part of the target name
+        for thang in fn:
 
-			sequence_re = re.compile('^\d{8}')  # Catches 00000000NoAutoDark, which is probably broken anyway
-			if (sequence_re.match(thang)):
-				continue
+            sequence_re = re.compile('^\d{8}')  # Catches 00000000NoAutoDark, which is probably broken anyway
+            if (sequence_re.match(thang)):
+                continue
 
-			exposure_re = re.compile('^([\d\.]+)secs$')
-			m = exposure_re.search(thang)
-			if (m):
-				image["exposure"] = m.group(0)
-				continue
+            exposure_re = re.compile('^([\d\.]+)secs$')
+            m = exposure_re.search(thang)
+            if (m):
+                image["exposure"] = m.group(0)
+                continue
 
-			binning_re = re.compile('^(\dx\d)$')
-			m = binning_re.fullmatch(thang)
-			if (m):
-				image["binning"] = m.group(0)
-				continue
+            binning_re = re.compile('^(\dx\d)$')
+            m = binning_re.fullmatch(thang)
+            if (m):
+                image["binning"] = m.group(0)
+                continue
 
-			filter_re = re.compile('^(LUMEN|B|V|R|I|RED|GREEN|BLUE|HA)$')
-			m = filter_re.fullmatch(thang)
-			if (m):
-				image["filter"] = m.group(0)
-				continue
+            filter_re = re.compile('^(LUMEN|B|V|R|I|RED|GREEN|BLUE|HA)$')
+            m = filter_re.fullmatch(thang)
+            if (m):
+                image["filter"] = m.group(0)
+                continue
 
-			datestr_re = re.compile('^(\d{4}-\d{2}-\d{2})$')
-			m = datestr_re.fullmatch(thang)
-			if (m):
-				image["fndate"] = m.group(0)
-				continue
+            datestr_re = re.compile('^(\d{4}-\d{2}-\d{2})$')
+            m = datestr_re.fullmatch(thang)
+            if (m):
+                image["fndate"] = m.group(0)
+                continue
 
-			# Anything left becomes target name
-			name.append(thang)
+            # Anything left becomes target name
+            name.append(thang)
 
-		# Now assemble the name
-		target = ' '.join(name)
-		if (target in self.ngc_catalog):
-			image["target"] = self.ngc_catalog[target]
-			image["altname"] = target
-		else:
-			image["target"] = target
+        # Now assemble the name
+        target = ' '.join(name)
+        if (target in self.ngc_catalog):
+            image["target"] = self.ngc_catalog[target]
+            image["altname"] = target
+        else:
+            image["target"] = target
 
-		#print(image)
-		return(image)
+        #print(image)
+        return(image)
 
-	def fits2png(self, image):
-		'''Generate png preview and thumbnail images.'''
-		preview = image["path"][:-5] + '.png'
-		if (self.forcepng or not os.path.exists(preview)):
-			cmd = 'fitspng -o \"{}\" \"{}\"'.format(preview, image["path"])
-			os.system(cmd)
+    def fits2png(self, image):
+        '''Generate png preview and thumbnail images.'''
+        # scaling is how much to reduce our image to get a 128x? thumbnail (or just slightly larger)
+        if ('binning' in image):
+            if (image['binning'] == '4x4'):
+                scaling = 8
+            elif (image['binning'] == '3x3'):
+                scaling = 11
+            elif (image['binning'] == '2x2'):
+                scaling = 17
+            elif (image['binning'] == '1x1'):
+                scaling = 35
+            else:
+                scaling = 35
+        else:
+            scaling = 35
 
-		thumb = image["path"][:-5] + '-thumb.png'
-		if (self.forcepng or not os.path.exists(thumb)):
-			cmd = 'fitspng -s 17 -o \"{}\" \"{}\"'.format(thumb, image["path"])
-			os.system(cmd)
+        preview = image["path"][:-5] + '.png'
+        if (self.forcepng or not os.path.exists(preview)):
+            cmd = 'fitspng -o \"{}\" \"{}\"'.format(preview, image["path"])
+            os.system(cmd)
 
-		return(preview, thumb)
+        thumb = image["path"][:-5] + '-thumb.png'
+        if (self.forcepng or not os.path.exists(thumb)):
+            cmd = 'fitspng -s {} -o \"{}\" \"{}\"'.format(scaling, thumb, image["path"])
+            os.system(cmd)
 
-	def findNewFits(self, path, fitsdb):
-		'''Find new FITS files since last time we were run.  Runs the `find` system command on `path` to
-		   locate *.fits newer than `ts_file`.
-		   TBD: Stash the results in a list and build indexes into the list.'''
-		cache_dir = '.'
-		ts_file = cache_dir + '/.last_run'
-		if (os.path.exists(ts_file)):
-			newer_arg = '-newer ' + ts_file
-		else:
-			newer_arg = '';
+        return(preview, thumb)
 
-		find_cmd = "find {} {} -name '*\.fits'".format(path, newer_arg)
-		print("DEBUG: " + find_cmd)
-		with os.popen(find_cmd) as find_out:
-			for fullpath in find_out:
-				fullpath = fullpath.rstrip()
-				path = fullpath.split('/')
-				filename = path.pop()
-				datestr = path.pop()
+    def findNewFits(self, path, fitsdb):
+        '''Find new FITS files since last time we were run.  Runs the `find` system command on `path` to
+           locate *.fits newer than `ts_file`.
+           TBD: Stash the results in a list and build indexes into the list.'''
+        cache_dir = '.'
+        ts_file = cache_dir + '/.last_run'
+        if (os.path.exists(ts_file)):
+            newer_arg = '-newer ' + ts_file
+        else:
+            newer_arg = '';
 
-				# We're only interested in folders that are dates
-				datestr_re = re.compile("(\d{4}-\d{2}-\d{2})");
-				m = datestr_re.search(datestr)
-				if (m):
-					image = self.parseFilename(filename)
-					image["date"] = m.group(0)
-					image["path"] = fullpath
-					print("\nPath: {}".format(fullpath))
-					(p,t) = self.fits2png(image)
-					image['preveiw'] = p
-					image['thumbnail'] = t
-					#dbstash(image)
-					fitsdb.insert(image)
-					print('  ', image)
+        find_cmd = "find {} {} -name '*\.fits'".format(path, newer_arg)
+        print("DEBUG: " + find_cmd)
+        with os.popen(find_cmd) as find_out:
+            for fullpath in find_out:
+                fullpath = fullpath.rstrip()
+                path = fullpath.split('/')
+                filename = path.pop()
+                datestr = path.pop()
 
-	def dbstash(self, image):
-		'''Stash `image` into imagedb and build appropriate indexes.'''
-		imagedb.append(image)
-		ndx = len(imagedb) - 1  # This is the primary key for indexes
+                # We're only interested in folders that are dates
+                datestr_re = re.compile("(\d{4}-\d{2}-\d{2})");
+                m = datestr_re.search(datestr)
+                if (m):
+                    image = self.parseFilename(filename)
+                    image["date"] = m.group(0)
+                    image["path"] = fullpath
+                    print("\nPath: {}".format(fullpath))
+                    (p,t) = self.fits2png(image)
+                    image['preview'] = p
+                    image['thumbnail'] = t
+                    #dbstash(image)
+                    fitsdb.insert(image)
+                    print('  ', image)
 
-		# Collect some shorthands to make indexing code more redable
-		date = image['date']
-		names = list()
-		names.append(image['target'])
-		if ('altname' in image):
-			names.append(image['altname'])
+    def dbstash(self, image):
+        '''Stash `image` into imagedb and build appropriate indexes.'''
+        imagedb.append(image)
+        ndx = len(imagedb) - 1  # This is the primary key for indexes
 
-		# Build date_ndx[date][name] = list of image ndx's
-		if (date not in date_ndx):
-			print("Initializing date_ndx[{}]".format(date))
-			date_ndx[date] = dict()
-		for name in names:
-			if (name not in date_ndx[date]):
-				print("Initializing date_ndx[{}][{}]".format(date,name))
-				date_ndx[date][name] = list()
-			date_ndx[date][name].append(ndx)
+        # Collect some shorthands to make indexing code more redable
+        date = image['date']
+        names = list()
+        names.append(image['target'])
+        if ('altname' in image):
+            names.append(image['altname'])
 
-		# Build name_ndx[name][date] = list of image ndx's
-		for name in names:
-			if (name not in name_ndx):
-				print("Initializing name_ndx[{}]".format(name))
-				name_ndx[name] = dict()
-			if (date not in name_ndx[name]):
-				print("Initializing name_ndx[{}][{}]".format(name,date))
-				name_ndx[name][date] = list()
-			name_ndx[name][date].append(ndx)
+        # Build date_ndx[date][name] = list of image ndx's
+        if (date not in date_ndx):
+            print("Initializing date_ndx[{}]".format(date))
+            date_ndx[date] = dict()
+        for name in names:
+            if (name not in date_ndx[date]):
+                print("Initializing date_ndx[{}][{}]".format(date,name))
+                date_ndx[date][name] = list()
+            date_ndx[date][name].append(ndx)
 
-	def loadOldDb():
-		pass
+        # Build name_ndx[name][date] = list of image ndx's
+        for name in names:
+            if (name not in name_ndx):
+                print("Initializing name_ndx[{}]".format(name))
+                name_ndx[name] = dict()
+            if (date not in name_ndx[name]):
+                print("Initializing name_ndx[{}][{}]".format(name,date))
+                name_ndx[name][date] = list()
+            name_ndx[name][date].append(ndx)
 
-	def saveNewDb():
-		print(json.dumps(imagedb, indent=4))
-		print(json.dumps(date_ndx, indent=4))
-		print(json.dumps(name_ndx, indent=4))
-		pass
+    def loadOldDb():
+        pass
+
+    def saveNewDb():
+        print(json.dumps(imagedb, indent=4))
+        print(json.dumps(date_ndx, indent=4))
+        print(json.dumps(name_ndx, indent=4))
+        pass
 
 if (__name__ == "__main__"):
 
-	fitsfiles = FitsFiles()
-	fitsdb = fitsdb.Fitsdb()
-	fitsfiles.findNewFits('fits', fitsdb)
-
-	print("\nname_ndx = ", name_ndx)
-	print("\ndate_ndx = ", date_ndx)
+    fitsfiles = FitsFiles()
+    fitsfiles.forcepng = 'True'  # Rebuild PNGs
+    fitsdb = fitsdb.Fitsdb()
+    fitsfiles.findNewFits('fits', fitsdb)
 
