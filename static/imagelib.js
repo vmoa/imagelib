76691b9c6930cc97a6d34ef0ba111d9e302bcf8d
David Kensiski <David@Kensiski.org>
2023-07-02 08:19:51 -0700

    added recolorSelect() callback; added comments; removed unused code

--- static/imagelib.js
+++ static/imagelib.js
@@ -4,19 +4,14 @@
 
 var color_selected     = '#ffbbba';
 var color_unselected   = 'gray';
+var color_downloading  = 'orange';
 
-var black        = 'black';
-var yellow       = 'yellow';
-var green        = '#08FF08';
-var light_green  = '#C0FFC0';
-var red          = '#FF0808';
-
-/* Global so keydownHandler can access */
+/* Element that is currently being previewed (global so that keydownHandler() can access) */
 var previewElement = 0;
 
-/* Mode: 0=none, 1=all, -1=toggle */
+/* Set `rfoIsSelected` attribute based on mode: 0=none, 1=all, -1=toggle */
 function select(el, mode) {
-    console.log("select("+el+","+mode+")");
+    /* console.log("select("+el+","+mode+")"); */
     var element = document.getElementById(el);
     if (mode == -1) {
         element.rfoIsSelected = ! element.rfoIsSelected;
@@ -26,10 +21,12 @@ function select(el, mode) {
     element.style.borderColor = element.rfoIsSelected ? color_selected : color_unselected;
 }
 
+/* Called from html via onclick() for thumbnail title */
 function toggle(el) {
     select(el, -1);
 }
 
+/* Call select() for each element whose id starts with `prefix`; see select() for `mode` */
 function selectMulti(prefix, mode) {
     console.log("selectMulti("+prefix+","+mode+")");
     var len = prefix.length;
@@ -41,23 +38,38 @@ function selectMulti(prefix, mode) {
     }
 }
 
+/* Time delayed callback to reset selection colors after download (see setRecids()) */
+function recolorSelect() {
+    console.log("recolorSelect()");
+    var thumbs = document.getElementsByClassName('thumb');
+    for (let i=0; i < thumbs.length; i++) {
+        thumbs[i].style.borderColor = thumbs[i].rfoIsSelected ? color_selected : color_unselected;
+    }
+}
+
+/* Onclick() function for download button: fill in the `recids` form field based on `rfoIsSelected` before submitting form */
 function setRecids() {
     console.log("setRecids()");
     var dlist = [];
     var thumbs = document.getElementsByClassName('thumb');
     for (let i=0; i < thumbs.length; i++) {
         if (thumbs[i].rfoIsSelected) {
-            dlist.push(thumbs[i].dataset.recid)
+            dlist.push(thumbs[i].dataset.recid);
+            thumbs[i].rfoIsSelected = 0;
+            thumbs[i].style.borderColor = color_downloading;
         }
     }
     console.log("recids=" + dlist.join(","));
     document.dlform.recids.value = dlist.join(",");
+    setTimeout(recolorSelect, 2000)
 }
 
+/* Reutrn just the filename component of a full path */
 function basename(path) {
    return path.split('/').reverse()[0];
 }
 
+/* Accept several keystrokes in our preview modal */
 function keydownHandler(event) {
     console.log("keydownHandler(" + event + ")");
     if (document.getElementById("preview-container").style.display == "block") {
@@ -69,6 +81,7 @@ function keydownHandler(event) {
     }
 }
 
+/* Preview modal: overlays current display with preview png */
 function preview(el) {
     console.log("preview("+el+")");
     previewElement = el;  /* make available to event handler */
@@ -86,11 +99,13 @@ function preview(el) {
     document.getElementById("preview-container").style.display = "block";
 }
 
+/* Special flavor of toggle() that also colors the preview border */
 function toggleSelect(el) {
     select(el, -1);
     document.getElementById("preview-content").style.borderColor = document.getElementById(el).rfoIsSelected ? color_selected : color_unselected;
 }
 
+/* Close the preview modal and return to our regularly scheduled display */
 function closePreview() {
     document.getElementById("preview-container").style.display = "none";
     document.removeEventListener("keydown", keydownHandler);
